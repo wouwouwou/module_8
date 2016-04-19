bac05745a81ff64d3173d91b46c23ba04e10145b
Wouter Bos <wouwouwou@users.noreply.github.com>
2016-04-19 14:27:04 +0200

    implemented scanner

--- pp/block1/cc/dfa/MyScanner.java
+++ pp/block1/cc/dfa/MyScanner.java
@@ -1,5 +1,6 @@
 package pp.block1.cc.dfa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +9,20 @@ import java.util.List;
 public class MyScanner implements Scanner{
     @Override
     public List<String> scan(State dfa, String text) {
-        return null;
+        List<String> res = new ArrayList<>();
+        String s = "";
+        State begin = dfa;
+        for(Character c : text.toCharArray()){
+            if (dfa.hasNext(c)) {
+                dfa = dfa.getNext(c);
+                s += c;
+            }
+            if (dfa.isAccepting()) {
+                res.add(s);
+                dfa = begin;
+                s = "";
+            }
+        }
+        return res;
     }
 }
