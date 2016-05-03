183baa4240ae35018552bc61843d80ba746b2431
Wouter Bos <wouwouwou@users.noreply.github.com>
2016-05-03 14:13:48 +0200

    Exercise 8 done

--- src/main/java/pp/block2/cc/ll/GenericLLParser.java
+++ src/main/java/pp/block2/cc/ll/GenericLLParser.java
@@ -2,21 +2,18 @@ package pp.block2.cc.ll;
 
 import java.util.*;
 
+import jdk.nashorn.internal.ir.Terminal;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.Token;
 
-import pp.block2.cc.AST;
-import pp.block2.cc.NonTerm;
-import pp.block2.cc.ParseException;
-import pp.block2.cc.Parser;
-import pp.block2.cc.Symbol;
-import pp.block2.cc.Term;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+import pp.block2.cc.*;
 
 /** Generic table-driven LL(1)-parser. */
 public class GenericLLParser implements Parser {
 	/** The grammar underlying this parser instance. */
 	private final Grammar g;
-	/** The LL-calculater for the grammar. */
+	/** The LL-calculator for the grammar. */
 	private final LLCalc calc;
 	/** Map from non-terminals to maps of rules indexed by terminal. */
 	private Map<NonTerm, Map<Term, Rule>> ll1Table;
@@ -24,6 +21,8 @@ public class GenericLLParser implements Parser {
 	private int index;
 	/** Token list of the currently parsed input. */
 	private List<? extends Token> tokens;
+    /** Symbolfactory for extracting a term from a token */
+    private SymbolFactory fact;
 
 
 	public GenericLLParser(Grammar g) {
@@ -35,6 +34,7 @@ public class GenericLLParser implements Parser {
 	public AST parse(Lexer lexer) throws ParseException {
 		this.tokens = lexer.getAllTokens();
 		this.index = 0;
+        this.fact = new SymbolFactory(lexer.getClass());
 		return parse(this.g.getStart());
 	}
 
@@ -51,7 +51,24 @@ public class GenericLLParser implements Parser {
 	 * because the token stream does not contain the expected symbols
 	 */
 	private AST parse(Symbol symb) throws ParseException {
-        return null;
+        AST res = null;
+        if (symb instanceof Term) {
+            if (index >= tokens.size()) {
+                throw new ParseException("Reached eof too soon");
+            }
+            if(symb != Symbol.EMPTY) {
+                try {
+                    res = new AST((Term) symb, tokens.get(index));
+                } catch (AssertionError e) {
+                    throw new ParseException("Token type not equal to symbol type!");
+                }
+                index++;
+            }
+        } else {
+            Rule r = lookup((NonTerm) symb);
+            res = parse(r);
+        }
+        return res;
 	}
 
 	/** Parses the start of the token stream according to a given
@@ -63,9 +80,15 @@ public class GenericLLParser implements Parser {
 	 * @throws ParseException if the symbol cannot be parsed
 	 * because the token stream does not contain the expected symbols
 	 */
-	private AST parse(Rule rule) throws ParseException {
-		// fill in
-        return null;
+	private AST parse(Rule r) throws ParseException {
+        AST res = new AST(r.getLHS());
+        int i = 0;
+        while (i < r.getRHS().size()) {
+            Symbol symbol = r.getRHS().get(i);
+            res.addChild(parse(symbol));
+            i++;
+        }
+        return res;
 	}
 
 	/** Uses the lookup table to look up the rule to which
@@ -135,12 +158,14 @@ public class GenericLLParser implements Parser {
             }
             res.put(A, innermap);
             for (Rule r : g.getRules()) {
-                Set<Term> firstpset = calc.getFirstp().get(r);
-                for (Term w : firstpset) {
-                    res.get(A).put(w, r);
-                }
-                if (firstpset.contains(Symbol.EOF)) {
-                    res.get(A).put(Symbol.EOF, r);
+                if (r.getLHS().equals(A)) {
+                    Set<Term> firstpset = calc.getFirstp().get(r);
+                    for (Term w : firstpset) {
+                        res.get(A).put(w, r);
+                    }
+                    if (firstpset.contains(Symbol.EOF)) {
+                        res.get(A).put(Symbol.EOF, r);
+                    }
                 }
             }
         }
