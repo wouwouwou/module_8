f92c9ec3ea059746dff0921e7f42016f2da85bc9
Erik Gaal <git@erikgaal.nl>
2016-05-11 11:01:35 +0200

    Aftekenen FP 3+4

--- src/main/haskell/series3/exercise3.hs
+++ src/main/haskell/series3/exercise3.hs
@@ -7,6 +7,11 @@ data Tree1a = Leaf1a Int
 data Tree1d = Leaf1d (Int, Int)
             | Node1d [Tree1d]
 
+
+pp1d :: Tree1d -> RoseTree
+pp1d (Leaf1d l) = RoseNode (show l) []
+pp1d (Node1d ts) = RoseNode "" (map pp1d ts)
+
 -- Exercise 3a
 binMirror :: Tree1a -> Tree1a
 binMirror (Leaf1a l) = Leaf1a l
@@ -15,4 +20,8 @@ binMirror (Node1a n t1 t2) = Node1a n t2 t1
 -- Exercise 3b
 binMirror' :: Tree1d -> Tree1d
 binMirror' (Leaf1d (i1, i2)) = Leaf1d (i2, i1)
-binMirror' (Node1d ts) = Node1d ts
+binMirror' (Node1d ts) = Node1d (map binMirror' (reverse ts))
+
+tree1 = Node1d [Leaf1d (1,2), (Node1d [Leaf1d (1,2), Leaf1d (5,6)]), Leaf1d (3,4)]
+
+ex = showTreeList [pp1d tree1, pp1d $ binMirror' tree1, pp1d $ binMirror' $ binMirror' tree1]
