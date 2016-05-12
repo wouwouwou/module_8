74b1344fb5be0bd55f874ac990b6b06760223d04
Wouter Bos <wouwouwou@users.noreply.github.com>
2016-05-12 10:44:05 +0200

    Added fp excercises, fixed some minor issues

--- src/main/haskell/series1/exercise2.hs
+++ src/main/haskell/series1/exercise2.hs
@@ -2,7 +2,7 @@ module S1E2 where
 import Data.Char
 
 code :: Int -> Char -> Char
-code n x | isLower x = chr (97 + (ord x + n) `mod` 123)
-         | isUpper x = chr (65 + (ord x + n) `mod` 91)
+code n x | isLower x = chr ((ord x + n) `mod` 123)
+         | isUpper x = chr ((ord x + n) `mod` 91)
          | otherwise = x
 -- map (code 3) "Tomorrow evening, 8 o'clock in Amsterdam"
--- src/main/haskell/series1/exercise5.hs
+++ src/main/haskell/series1/exercise5.hs
@@ -1,7 +1,7 @@
 module S1E5 where
 
 extrX :: (Floating a) => a -> a -> a -> a
-extrX a b _ = (-b) / (2 * a)
+extrX a b _ = -b / (2 * a)
 
 extrY :: (Floating a) => a -> a -> a -> a
 extrY a b c = a * x ^ 2 + b * x + c
