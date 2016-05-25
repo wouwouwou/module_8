2c23d686b20f69b320a645b6ad03f564701ce01a
Wouter Bos <wouwouwou@users.noreply.github.com>
2016-05-25 11:32:02 +0200

    Finished 4.1 and 4.2 cc

--- src/main/java/pp/block4/cc/cfg/BottomUpCFGBuilder.java
+++ src/main/java/pp/block4/cc/cfg/BottomUpCFGBuilder.java
@@ -3,6 +3,8 @@ package pp.block4.cc.cfg;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
@@ -11,15 +13,17 @@ import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import pp.block4.cc.ErrorListener;
-import pp.block4.cc.cfg.FragmentParser.BlockStatContext;
-import pp.block4.cc.cfg.FragmentParser.ContStatContext;
-import pp.block4.cc.cfg.FragmentParser.BreakStatContext;
+import pp.block4.cc.cfg.FragmentParser.*;
 
 /** Template bottom-up CFG builder. */
 public class BottomUpCFGBuilder extends FragmentBaseListener {
 	/** The CFG being built. */
 	private Graph graph;
+
+    private ParseTreeProperty<Node[]> nodes = new ParseTreeProperty<>();
 
 	/** Builds the CFG for a program contained in a given file. */
 	public Graph build(File file) {
@@ -52,19 +56,72 @@ public class BottomUpCFGBuilder extends FragmentBaseListener {
 	/** Builds the CFG for a program given as an ANTLR parse tree. */
 	public Graph build(ParseTree tree) {
 		this.graph = new Graph();
-		// TODO Fill in
-		throw new UnsupportedOperationException("Fill in");
+        new ParseTreeWalker().walk(this, tree);
+		return this.graph;
 	}
 
-	@Override
-	public void enterBreakStat(BreakStatContext ctx) {
-		throw new IllegalArgumentException("Break not supported");
-	}
+    @Override
+    public void exitProgram(ProgramContext ctx) {
+        for (int i = 0; i < ctx.stat().size() - 1; i++) {
+            Node[] stat1 = nodes.get(ctx.stat(i));
+            Node[] stat2 = nodes.get(ctx.stat(i + 1));
+            stat1[1].addEdge(stat2[0]);
+        }
+    }
 
-	@Override
-	public void enterContStat(ContStatContext ctx) {
-		throw new IllegalArgumentException("Continue not supported");
-	}
+    @Override
+    public void exitDecl(DeclContext ctx) {
+        Node n = addNode(ctx, "decl");
+        nodes.put(ctx, new Node[] {n,n});
+    }
+
+    @Override
+    public void exitAssignStat(AssignStatContext ctx) {
+        Node n = addNode(ctx, "assign");
+        nodes.put(ctx, new Node[] {n,n});
+    }
+
+    @Override
+    public void exitIfStat(IfStatContext ctx) {
+        Node expr = addNode(ctx.expr(), "if");
+        Node ifexit = addNode(ctx, "ifexit");
+        for (StatContext s : ctx.stat()) {
+            Node[] thenelse = nodes.get(s);
+            expr.addEdge(thenelse[0]);
+            thenelse[1].addEdge(ifexit);
+        }
+        if (ctx.stat().size() < 2) {
+            expr.addEdge(ifexit);
+        }
+        nodes.put(ctx, new Node[]{expr, ifexit});
+    }
+
+    @Override
+    public void exitWhileStat(WhileStatContext ctx) {
+        Node expr = addNode(ctx.expr(), "whilestat");
+        Node[] whilenode = nodes.get(ctx.stat());
+        expr.addEdge(whilenode[0]);
+        whilenode[1].addEdge(expr);
+        Node exitnode = addNode(ctx.expr(), "whileExit");
+        expr.addEdge(exitnode);
+        nodes.put(ctx, new Node[] {expr, exitnode});
+    }
+
+    @Override
+    public void exitBlockStat(BlockStatContext ctx) {
+        for (int i = 0; i < ctx.stat().size() - 1; i++) {
+            Node[] stat1 = nodes.get(ctx.stat(i));
+            Node[] stat2 = nodes.get(ctx.stat(i + 1));
+            stat1[1].addEdge(stat2[0]);
+        }
+        nodes.put(ctx, new Node[] {nodes.get(ctx.stat(0))[0], nodes.get(ctx.stat(ctx.stat().size() - 1))[1]});
+    }
+
+    @Override
+    public void exitPrintStat(PrintStatContext ctx) {
+        Node n = addNode(ctx, "print");
+        nodes.put(ctx, new Node[]{n, n});
+    }
 
 	/** Adds a node to he CGF, based on a given parse tree node.
 	 * Gives the CFG node a meaningful ID, consisting of line number and 
@@ -82,7 +139,16 @@ public class BottomUpCFGBuilder extends FragmentBaseListener {
 		}
 		BottomUpCFGBuilder builder = new BottomUpCFGBuilder();
 		for (String filename : args) {
-			File file = new File(filename);
+            URI path = null;
+            try {
+                path = BottomUpCFGBuilder.class.getResource(filename).toURI();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+            if (path == null) {
+                throw new NullPointerException();
+            }
+            File file = new File(path);
 			System.out.println(filename);
 			System.out.println(builder.build(file));
 		}
